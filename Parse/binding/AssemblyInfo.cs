164678b60e8fd08ddfced6ae43cf87f739e1cc7f
Jared Hendry <hendry.jared@gmail.com>
2016-07-23 14:52:04 -0400

    [Parse] Stop linking AdSupport framework

--- Parse/binding/AssemblyInfo.cs
+++ Parse/binding/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System;
 using MonoTouch.ObjCRuntime;
-[assembly: LinkWith ("Parse", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, LinkerFlags = "-lsqlite3 -lz",Frameworks = "MobileCoreServices CoreGraphics SystemConfiguration AudioToolbox Security StoreKit AssetsLibrary CFNetwork CoreLocation QuartzCore", WeakFrameworks="AdSupport Social Accounts", ForceLoad = true)]
+[assembly: LinkWith ("Parse", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, LinkerFlags = "-lsqlite3 -lz",Frameworks = "MobileCoreServices CoreGraphics SystemConfiguration AudioToolbox Security StoreKit AssetsLibrary CFNetwork CoreLocation QuartzCore", WeakFrameworks="Social Accounts", ForceLoad = true)]
